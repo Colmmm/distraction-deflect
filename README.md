63af14c4576eaf8bf4962134d1a86dc657230e86
Colm Sam <colmwaikeat@gmail.com>
2024-07-17 15:39:27 +0100

    adding basic structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Distraction Deflector
+
+![Logo](logo.jpg)
+
+Distraction Deflector is a productivity browser extension designed to help users stay focused by redirecting them away from websites they find distracting. By reflecting and blocking access to designated sites, it enhances productivity and supports efficient work habits.
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'src/popup.html',
+        background: 'src/background.js'
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    }
+  }
+});
+
